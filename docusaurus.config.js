7d800bf7fe679b14719c4bca0b88496100756643
Ronald Pereira <rpereira@taecontrol.com>
2023-01-30 18:28:30 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,8 +7,8 @@ const darkCodeTheme = require("prism-react-renderer/themes/palenight");
 const config = {
   title: "Larastats",
   tagline: "Don't miss your site status.",
-  url: "https://taecontrol.github.io/",
-  baseUrl: "/larastats-docs/",
+  url: "https://docs.larastats.dev/",
+  baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
